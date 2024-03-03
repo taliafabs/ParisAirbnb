f7693c8c8547334abfa44f5e2a7002523d22b6f1
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-03-03 23:16:43 +0000

    clean up starter folder

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -7,20 +7,30 @@
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
 
-
 #### Workspace setup ####
-library(opendatatoronto)
+library(janitor)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+url <-
+  paste0(
+    "http://data.insideairbnb.com/france/ile-de-france/", 
+    "paris/2023-12-12/data/listings.csv.gz"
+  )
 
+paris_airbnb_data <-
+  read_csv(
+    file = url,
+    guess_max = 20000
+  )
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(paris_airbnb_data, "data/raw_data/paris_airbnb_data.csv")
 
-         
+paris_airbnb_data
+
+
+
+
--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,59 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Clean the raw Paris airbnb data
+# Author: Talia Fabregas
+# Date: 3 March 2024
+# Contact: talia.fabregas@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Run 01-download_data.R first
+# Any other information needed?
 
 #### Workspace setup ####
-library(tidyverse)
+library(arrow)
+library(janitor)
 
-#### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+paris_airbnb_data <- read_csv("data/raw_data/paris_airbnb_data.csv")
+paris_airbnb_data_selected <-
+  paris_airbnb_data |>
+  select(
+    host_id,
+    host_response_time,
+    host_is_superhost,
+    host_total_listings_count,
+    neighbourhood_cleansed,
+    bathrooms,
+    bedrooms,
+    price,
+    number_of_reviews,
+    review_scores_rating,
+    review_scores_accuracy,
+    review_scores_value
+  )
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
+paris_airbnb_data_selected$price |>
+  str_split("") |>
+  unlist() |>
+  unique()
+
+paris_airbnb_data_selected |>
+  select(price) |>
+  filter(str_detect(price, ","))
+
+paris_airbnb_data_selected <-
+  paris_airbnb_data_selected |>
   mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+    price = str_remove_all(price, "[\\$,]"),
+    price = as.integer(price)
+  )
+
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_parquet(
+  x = paris_airbnb_data_selected, 
+  sink = 
+    "2023-12-12-paris-airbnblistings-select_variables.parquet"
+)
+
+rm(paris_airbnb_data)
+
+
+
