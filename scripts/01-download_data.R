daafdad80c77e0e7edc0d5486896cd92a7eabe8c
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-03-04 18:38:28 +0000

    commit

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Airbnb
+# Author: Talia Fabregas
+# Date: 3 March 2024 
+# Contact: talia.fabregas@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+
 
 #### Workspace setup ####
 library(janitor)
